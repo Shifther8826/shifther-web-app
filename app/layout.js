e431a3cfc7de84b573ddeff6249989334dec5434
Shifther8826 <info@michelleshifts.com>
2026-04-23 22:37:48 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import { ClerkProvider } from "@clerk/nextjs";
+
 export const metadata = {
   title: "SHIFTHer",
   description: "SHIFTHer web app",
@@ -5,8 +7,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+     <ClerkProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
